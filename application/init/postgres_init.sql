a18cdbe3d7131b286373f4391e3e0f3dd12b7643
Vincent <meijer.vincent@gmail.com>
2017-04-10 17:45:00 -0400

    DEV-266 Fix incorrect merge

--- application/init/postgres_init.sql
+++ application/init/postgres_init.sql
@@ -32,31 +32,6 @@ CREATE TABLE temp_table_bird_selection (
 
 CREATE INDEX temp_dead_birds_projected_locationsidx ON temp_table_bird_selection USING GIST ( location );
 
-CREATE TABLE human_cases (
-    human_case_id integer PRIMARY KEY,
-    onset_date date,
-    days_lit integer,
-    days_before integer
-);
-
-CREATE TABLE human_cases_unprojected (
-) INHERITS (human_cases);
-
-CREATE TABLE human_cases_projected (
-) INHERITS (human_cases);
-
-ALTER TABLE human_cases_unprojected ADD CONSTRAINT human_cases_unprojected_pkey PRIMARY KEY (human_case_id);
-
-ALTER TABLE human_cases_projected ADD CONSTRAINT human_cases_projected_pkey PRIMARY KEY (human_case_id);
-
-SELECT AddGeometryColumn('public', 'human_cases_unprojected', 'location', 4269, 'POINT', 2);
-
-SELECT AddGeometryColumn('public', 'human_cases_projected', 'location', 54003, 'POINT', 2);
-
-CREATE INDEX human_cases_unprojected_locationsidx ON human_cases_unprojected USING GIST ( location );
-
-CREATE INDEX human_cases_projected_locationsidx ON human_cases_projected USING GIST ( location );
-
 CREATE TABLE effects_polys (
     tile_id integer PRIMARY KEY,
     county smallint
@@ -99,25 +74,6 @@ SELECT AddGeometryColumn('public', 'effects_poly_centers_unprojected', 'the_geom
 
 -- SELECT AddGeometryColumn('public', 'effects_poly_centers_projected', 'the_geom', 29193, 'POINT', 2);
 
-CREATE TABLE analysis_areas (
-    id integer PRIMARY KEY,
-    name varchar(80)
-);
-
-CREATE TABLE analysis_areas_unprojected (
-) INHERITS (analysis_areas);
-
-CREATE TABLE analysis_areas_projected (
-) INHERITS (analysis_areas);
-
-ALTER TABLE analysis_areas_unprojected ADD CONSTRAINT analysis_areas_unprojected_pkey PRIMARY KEY (id);
-
-ALTER TABLE analysis_areas_projected ADD CONSTRAINT analysis_areas_projected_pkey PRIMARY KEY (id);
-
-SELECT AddGeometryColumn('public', 'analysis_areas_unprojected', 'the_geom', 29193, 'MULTIPOLYGON', 2);
-
-SELECT AddGeometryColumn('public', 'analysis_areas_projected', 'the_geom', 29193, 'MULTIPOLYGON', 2);
-
 CREATE TABLE risk_table_list (
     table_id integer PRIMARY KEY,
     tablename varchar(40),
@@ -134,33 +90,15 @@ CREATE TABLE risk_table_parent (
     nmcm float
 );
 
-CREATE TABLE all_risk (
-    tile_id integer REFERENCES effects_polys_unprojected (tile_id),
-    risk integer,
-    disp_val float,
-    date date
-);
-
-CREATE TABLE dist_margs_params (
-    param_id serial PRIMARY KEY,
-    close_space_param float,
-    close_time_param integer,
-    spatial_domain_param float,
-    temporal_domain_param integer
-);
-
 CREATE TABLE dist_margs (
-    param_id integer REFERENCES dist_margs_params (param_id),
     number_of_birds integer,
     close_pairs integer,
     probability float,
     cumulative_probability float,
     close_space integer,
-    close_time integer,
-    CONSTRAINT dist_margs_pkey PRIMARY KEY (param_id, number_of_birds, close_pairs, close_space, close_time)
+    close_time integer
 );
 
-CREATE INDEX dist_margs_paridx ON dist_margs (param_id);
 CREATE INDEX dist_margs_numidx ON dist_margs (number_of_birds);
 CREATE INDEX dist_margs_cpidx ON dist_margs (close_pairs);
 CREATE INDEX dist_margs_csidx ON dist_margs (close_space);
@@ -202,24 +140,24 @@ CREATE OR REPLACE FUNCTION
     LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION
-    nmcm(parameters_id integer, num_birds integer, cp integer, cs integer, ct integer)
+    nmcm(num_birds integer, cp integer, cs integer, ct integer)
     RETURNS float AS 
     $$
     DECLARE
         result_prob float;
         result_ct integer;
     BEGIN
-    SELECT cumulative_probability INTO result_prob FROM dist_margs WHERE param_id = parameters_id and number_of_birds = num_birds and close_pairs = cp and close_space = cs and close_time = ct;
+    SELECT cumulative_probability INTO result_prob FROM dist_margs WHERE number_of_birds = num_birds and close_pairs = cp and close_space = cs and close_time = ct;
     IF FOUND THEN
         RETURN result_prob;
     ELSE
         -- First find out if there are any, before going to the trouble of sorting:
-        SELECT close_time INTO result_ct FROM dist_margs WHERE param_id = parameters_id and number_of_birds = num_birds and close_pairs >= cp and close_time >= ct;
+        SELECT close_time INTO result_ct FROM dist_margs WHERE number_of_birds = num_birds and close_pairs >= cp and close_time >= ct;
         IF FOUND THEN
             -- Now actually find out which close_time to use:
-            SELECT close_time INTO result_ct FROM dist_margs WHERE param_id = parameters_id and number_of_birds = num_birds and close_pairs >= cp and close_time >= ct ORDER BY close_time LIMIT 1;
+            SELECT close_time INTO result_ct FROM dist_margs WHERE number_of_birds = num_birds and close_pairs >= cp and close_time >= ct ORDER BY close_time LIMIT 1;
             -- Then use that close_time in this query:
-            SELECT cumulative_probability INTO result_prob FROM dist_margs WHERE param_id = parameters_id and number_of_birds = num_birds and close_pairs >= cp and close_time = result_ct and close_space >= cs ORDER BY close_space LIMIT 1;
+            SELECT cumulative_probability INTO result_prob FROM dist_margs WHERE number_of_birds = num_birds and close_pairs >= cp and close_time = result_ct and close_space >= cs ORDER BY close_space LIMIT 1;
             IF FOUND THEN
                 RETURN result_prob;
             ELSE
