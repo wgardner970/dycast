1a79f2f184c501c9cf29e3464516401faf43c5e6
Vincent <meijer.vincent@gmail.com>
2017-05-12 15:47:40 -0400

    DEV_280 Update unit tests to reflect changes in coordinate sysmte use. Fix unit test parameters.

--- application/tests/dycast_test.py
+++ application/tests/dycast_test.py
@@ -14,21 +14,18 @@ class TestDycastFunctions(unittest.TestCase):
         config = config_service.get_default_config()
         dycast.read_config(None, config)
         dycast.init_db()
+        system_coordinate_system = config.get("dycast", "system_coordinate_system")
 
-        srid = "29193"
+        user_coordinate_system = "29193"
         extent_min_x = 197457.283284349
         extent_min_y = 7639474.3256114
-        extent_max_x = 197458.283284349
-        extent_max_y = 7639475.3256114
+        extent_max_x = 198056.722079
+        extent_max_y = 7639344.265401
 
-        gridpoints = grid_service.generate_grid(
-            srid, extent_min_x, extent_min_y, extent_max_x, extent_max_y)
+        gridpoints = grid_service.generate_grid(user_coordinate_system, system_coordinate_system, extent_min_x, extent_min_y, extent_max_x, extent_max_y)
 
         case_table = config.get("database", "dead_birds_table_projected")
         point = gridpoints[0]
-        projection_from = "29193"
-        projection_to = "29193"
-        count = dycast.get_vector_count_for_point(
-            case_table, point, projection_from, projection_to)
+        count = dycast.get_vector_count_for_point(case_table, point)
 
         self.assertIsNotNone(count)
--- application/tests/grid_service_test.py
+++ application/tests/grid_service_test.py
@@ -1,15 +1,19 @@
 import unittest
 from application.services import grid_service
-
+from application.services import config_service
 
 class TestGridService(unittest.TestCase):
 
     def test_generate_grid(self):
-        srid = "29193"
-        extent_mix_x = 197457.283284349
-        extent_max_x = 197957.283284349
-        extent_min_y = 7639474.3256114
-        extent_max_y = 7639974.3256114
+        config = config_service.get_default_config()
+        system_coordinate_system = config.get("dycast", "system_coordinate_system")
 
-        gridpoints = grid_service.generate_grid(srid, extent_mix_x, extent_min_y, extent_max_x, extent_max_y)
+        user_coordinate_system = "29193"
+        extent_min_x = 197457.283284349
+        extent_min_y = 7639474.3256114
+        extent_max_x = 198056.722079
+        extent_max_y = 7639344.265401
+
+        gridpoints = grid_service.generate_grid(user_coordinate_system, system_coordinate_system, extent_min_x, extent_min_y, extent_max_x, extent_max_y)
         self.assertIsNotNone(gridpoints)
+        self.assertGreaterEqual(len(gridpoints), 1)
