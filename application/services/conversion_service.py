0609dacc6760107348473af8efc193d6f2ff7cf5
Vincent <meijer.vincent@gmail.com>
2017-06-06 10:26:21 +0200

    DEV-179 Add service to convert stuff, such as dates to strings

---
+++ application/services/conversion_service.py
@@ -0,0 +1,8 @@
+
+def get_string_from_date_object(date):
+    try:
+        return date.strftime('%Y-%m-%d')
+    except Exception as e:
+        logging.error(e)
+        logging.error("Couldn't convert date to string: %s", date)
+        raise
