8472bf0798a2ba5122cd5a443529a4ef53eb28bc
Vincent <meijer.vincent@gmail.com>
2017-09-13 18:21:30 -0400

    Put enable_debug() in a try/catch clause, so it can be loaded multiple times

--- application/services/debug_service.py
+++ application/services/debug_service.py
@@ -2,12 +2,17 @@ import os
 import logging
 import config_service
 
+
 def enable_debugger():
     if config_service.get_env_variable("DEBUG") == "True":
         import ptvsd
         secret = config_service.get_env_variable("REMOTE_DEBUG_SECRET")
-        ptvsd.enable_attach(secret=secret, address=('0.0.0.0', 3000))
-        print "Debugger is ready for attachment."
+        
+        try:
+            ptvsd.enable_attach(secret=secret, address=('0.0.0.0', 3000))
+            print "Debugger is ready for attachment."
+        except:
+            pass
 
         if config_service.get_env_variable("REMOTE_DEBUG") == "True":
             print "Waiting for debugger to attach..."
