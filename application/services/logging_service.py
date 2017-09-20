5254ca76c9814b547b96e6c67bc2394287d2bc41
Vincent <meijer.vincent@gmail.com>
2017-09-20 14:21:51 -0400

    Use config global var

--- application/services/logging_service.py
+++ application/services/logging_service.py
@@ -5,6 +5,8 @@ import ast
 import config_service
 
 
+CONFIG = config_service.get_config()
+
 
 def init_logging():
     root_logger = logging.getLogger()
@@ -25,15 +27,13 @@ def init_logging():
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
 
-
 def show_current_parameter_set():
-    config = config_service.get_config()
     logging.info("")
     logging.info("Using parameter set:")
-    logging.info("Spatial domain: %s", config.get("dycast", "spatial_domain"))
-    logging.info("Temporal domain: %s", config.get("dycast", "temporal_domain"))
-    logging.info("Close in space: %s", config.get("dycast", "close_in_space"))
-    logging.info("Close in time: %s", config.get("dycast", "close_in_time"))
+    logging.info("Spatial domain: %s", CONFIG.get("dycast", "spatial_domain"))
+    logging.info("Temporal domain: %s", CONFIG.get("dycast", "temporal_domain"))
+    logging.info("Close in space: %s", CONFIG.get("dycast", "close_in_space"))
+    logging.info("Close in time: %s", CONFIG.get("dycast", "close_in_time"))
     logging.info("")
 
 
@@ -48,4 +48,4 @@ def get_log_level():
         return logging.INFO
 
 def get_log_file_path():
-    return config.get("system", "logfile")
+    return CONFIG.get("system", "logfile")
