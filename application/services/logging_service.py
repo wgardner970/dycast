d26c9ee62ad9d147fde36b0b4d8b4f75a24baf5f
Vincent <meijer.vincent@gmail.com>
2017-09-20 14:21:04 -0400

    DEV-377 Fix for logging (again) not using the correct format. Now take existing handler and apply format to that

--- application/services/logging_service.py
+++ application/services/logging_service.py
@@ -8,13 +8,19 @@ import config_service
 
 def init_logging():
     root_logger = logging.getLogger()
+
     log_format = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+    log_formatter = logging.Formatter(log_format)
+
     log_level = get_log_level()
     root_logger.setLevel(log_level)
-
     logging.basicConfig(level=log_level, format=log_format)
 
-    log_formatter = logging.Formatter(log_format)
+    # Set formatter on existing handler
+    for handler in root_logger.handlers[:]:
+        handler.setFormatter(log_formatter)
+
+    # Create new handler to log to a file
     file_handler = logging.FileHandler(get_log_file_path())
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
