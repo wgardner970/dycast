4c97409c1e393b2ee276ac257e079cf8c03df7be
Vincent Meijer <meijer.vincent@gmail.com>
2018-01-18 15:45:06 +0100

    Add sqlalchemy models

--- application/models/models.py
+++ application/models/models.py
@@ -37,3 +37,32 @@ class DistributionMargins(DeclarativeBase):
     cumulative_probability = Column(Float)
     close_space = Column(Integer, primary_key=True, index=True)
     close_time = Column(Integer, primary_key=True, index=True)
+
+class TmpDailyCaseSelection(DeclarativeBase):
+    """SQLAlchemy Daily Case Selection model"""
+    __tablename__ = "tmp_daily_case_selection"
+
+    case_id = Column(Integer, primary_key=True)
+    report_date = Column(DateTime)
+    location = Column(Geometry(geometry_type='POINT', srid='3857'))
+
+class TmpClusterPerPointSelection(DeclarativeBase):
+    """SQLAlchemy Cluster Per Point Selection model"""
+    __tablename__ = "tmp_cluster_per_point_selection"    
+
+    case_id = Column(Integer, primary_key=True)
+    report_date = Column(DateTime)
+    location = Column(Geometry(geometry_type='POINT', srid='3857'))
+
+class Risk(DeclarativeBase):
+    """SQLAlchemy Risk model"""
+    __tablename__ = "risk"
+
+    risk_date = Column(DateTime, primary_key=True)
+    lat = Column(Float, primary_key=True)
+    long = Column(Float, primary_key=True)
+    num_birds = Column(Integer)
+    close_pairs = Column(Integer)
+    close_space = Column(Integer)
+    close_time = Column(Integer)
+    nmcm = Column(Float)
