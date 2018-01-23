5dda770a583407ab4c9b9f885fd4e11e9366e1cd
Vincent Meijer <meijer.vincent@gmail.com>
2018-01-23 18:43:03 +0100

    Add get_close_space_and_time() function. Add unit test

--- application/services/geography_service.py
+++ application/services/geography_service.py
@@ -6,18 +6,32 @@ from geoalchemy2.elements import WKTElement
 from application.services import database_service
 from application.models.models import Case
 
+
+
 def get_point_from_lat_long(lat, lon, projection):
     return WKTElement("POINT({0} {1})".format(lon, lat), srid=projection)
 
+
 def transform_point(point, target_projection):
     return ST_Transform(point, int(target_projection))
+
+
+def get_close_space_and_time(cases_in_cluster_query, close_in_space, close_in_time):
+    subquery = cases_in_cluster_query.subquery()
+    query = cases_in_cluster_query.join(subquery, literal(True)) \
+        .filter(func.ST_DWithin(Case.location, subquery.c.location, close_in_space),
+                func.abs(Case.report_date - subquery.c.report_date) <= close_in_time,
+                Case.id < subquery.c.id)
+
+    return database_service.get_count_for_query(query)
+
+
 def get_close_space_only(cases_in_cluster_query, close_in_space):
     subquery = cases_in_cluster_query.subquery()
     query = cases_in_cluster_query.join(subquery, literal(True)) \
         .filter(func.ST_DWithin(Case.location, subquery.c.location, close_in_space),
-                Case.id < subquery.c.id
-               )
-    
+                Case.id < subquery.c.id)
+
     return database_service.get_count_for_query(query)
 
 def get_close_time_only(cases_in_cluster_query, close_in_time):
