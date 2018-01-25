db1718c4de45532bad02d95f29aa57647bb69d10
Vincent Meijer <meijer.vincent@gmail.com>
2018-01-25 13:14:29 +0100

    Merge grid_service with geography_service

--- application/services/geography_service.py
+++ application/services/geography_service.py
@@ -1,6 +1,13 @@
+import logging
+import shapely.geometry
+import pyproj
 from geoalchemy2.functions import ST_Transform
 from geoalchemy2.elements import WKTElement
 
+from application.services import config_service
+
+
+CONFIG = config_service.get_config()
 
 
 
@@ -12,7 +19,49 @@ def transform_point(point, target_projection):
     return ST_Transform(point, int(target_projection))
 
 
+def generate_grid(dycast_parameters):
+    '''
+    Returns a raster grid with points in the coordinate system as
+    specified in global setting 'system_coordinate_system'
+    '''
 
+    srid_of_extent = dycast_parameters.srid_of_extent
+    extent_min_x = dycast_parameters.extent_min_x
+    extent_min_y = dycast_parameters.extent_min_y
+    extent_max_x = dycast_parameters.extent_max_x
+    extent_max_y = dycast_parameters.extent_max_y
 
+    system_coordinate_system = CONFIG.get("dycast", "system_coordinate_system")
+    
+    # Set up projections
+    projection_user_defined = pyproj.Proj(init="epsg:%s" % srid_of_extent)
+    projection_metric = pyproj.Proj(init='epsg:3857')  # metric; same as EPSG:900913
+    projection_system_default = pyproj.Proj(init="epsg:%s" % system_coordinate_system)
 
+    # Create corners of rectangle to be transformed to a grid
+    north_west = shapely.geometry.Point((extent_min_x, extent_min_y))
+    south_east = shapely.geometry.Point((extent_max_x, extent_max_y))
 
+    stepsize = 100  # 100 meter grid step size
+
+    # Project corners to target projection
+    # Transform NW and SE points to 3857
+    start = pyproj.transform(projection_user_defined, projection_metric, north_west.x, north_west.y)
+    end = pyproj.transform(projection_user_defined, projection_metric, south_east.x, south_east.y)
+
+    # Iterate over 2D area
+    gridpoints = []
+    x = start[0]
+    logging.info("Started generating grid...")
+    while x < end[0]:
+        y = start[1]
+        while y > end[1]:
+            transformed_coordinate = pyproj.transform(projection_metric, projection_system_default, x, y)
+            point = shapely.geometry.Point(transformed_coordinate)
+            gridpoints.append(point)
+            y -= stepsize
+        x += stepsize
+
+    logging.info("Done generating grid. Result: %s points", len(gridpoints))
+
+    return gridpoints
--- application/tests/geography_service_test.py
+++ application/tests/geography_service_test.py
@@ -1,17 +1,25 @@
 import unittest
-import datetime
 
-from application.services import risk_service as risk_service_module
-from application.services import database_service
 from application.services import geography_service
-from application.services import grid_service
+from application.models.classes import dycast_parameters
 from application.tests import test_helper_functions
 
 
 test_helper_functions.init_test_environment()
 
 
+
 class TestGeographyServiceFunctions(unittest.TestCase):
 
+    def test_generate_grid(self):
+        dycast_paramaters = dycast_parameters.DycastParameters()
 
-    
+        dycast_paramaters.srid_of_extent = "29193"
+        dycast_paramaters.extent_min_x = 197457.283284349
+        dycast_paramaters.extent_min_y = 7639474.3256114
+        dycast_paramaters.extent_max_x = 198056.722079
+        dycast_paramaters.extent_max_y = 7639344.265401
+
+        gridpoints = geography_service.generate_grid(dycast_paramaters)
+        self.assertIsNotNone(gridpoints)
+        self.assertGreaterEqual(len(gridpoints), 1)
