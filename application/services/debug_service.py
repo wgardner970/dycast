e1fcaadb751e8cef3d791bb7c35cb039658e6d01
Vincent Meijer <meijer.vincent@gmail.com>
2018-03-14 15:40:20 +0100

    Use new env variable to enable remote debugging (so it is not used by default when DEBUG is set)

--- application/services/debug_service.py
+++ application/services/debug_service.py
@@ -5,21 +5,22 @@ from application.services import config_service
 
 
 def enable_debugger():
-    if config_service.get_env_variable("DEBUG") == "True":
+    if config_service.get_env_variable("REMOTE_DEBUG") == "True":
+        logging.info("REMOTE_DEBUG == True  --> Attaching to remote debugger...")
         import ptvsd
         secret = config_service.get_env_variable("REMOTE_DEBUG_SECRET")
         
         try:
             ptvsd.enable_attach(secret=secret, address=('0.0.0.0', 3000))
-            print "Debugger is ready for attachment."
+            logging.info("Debugger is ready for attachment.")
         except:
             pass
 
 
         if config_service.get_env_variable("WAIT_FOR_ATTACH") == "True":
-            print "Waiting 10 seconds for debugger to attach..."
+            logging.info("Waiting 10 seconds for debugger to attach...")
             time.sleep(10)
             if ptvsd.is_attached:
-                print "Attached debugger"
+                logging.info("Attached debugger")
             else:
-                print "Not attached"
+                logging.info("Not attached")
