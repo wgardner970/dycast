48888734f23b9d173a61e96f93484a71a5ff3367
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-26 18:34:25 +0200

    #68 Fix logging (remove alembic config that overwrote dycast config)

--- application/init/migrations/env.py
+++ application/init/migrations/env.py
@@ -1,32 +1,19 @@
 from __future__ import with_statement
-from logging.config import fileConfig
-from alembic import context
-from application.services import database_service
-from application.services import config_service
-from application.models.models import DeclarativeBase
 
+from alembic import context
+
+from application.models.models import DeclarativeBase
+from application.services import config_service
+from application.services import database_service
 
 # Init Dycast config
 config_service.init_config()
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = DeclarativeBase.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
@@ -66,6 +53,7 @@ def run_migrations_online():
 
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
