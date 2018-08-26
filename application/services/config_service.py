000e7d0a4df2aebccb15e75ffe70bf8291cb2e65
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-26 18:00:28 +0200

    #68 Add default config path to init command. Only init when not already inited. Add additional convenience path methods

--- application/services/config_service.py
+++ application/services/config_service.py
@@ -5,27 +5,41 @@ import logging
 
 CONFIG = ConfigParser.SafeConfigParser(os.environ)
 
+
 def get_env_variable(var_name):
     try:
         return os.environ[var_name]
     except KeyError:
         return None
 
-def init_config(config_file_path):
-    try:
-        logging.debug("Reading config file: %s", config_file_path)
-        CONFIG.read(config_file_path)
-        logging.debug("Done reading config file.")
-    except Exception:
-        logging.exception("Could not read config file: %s", config_file_path)
-        sys.exit()
+
+def init_config(config_file_path=None):
+    if CONFIG.sections().__len__() == 0:
+        try:
+            if not config_file_path or config_file_path == "":
+                config_file_path = get_default_config_file_path()
+            logging.debug("Reading config file: %s", config_file_path)
+            CONFIG.read(config_file_path)
+            logging.debug("Done reading config file.")
+        except Exception:
+            logging.exception("Could not read config file: %s", config_file_path)
+            sys.exit()
+    else:
+        logging.debug("Config already initialized, skipping")
+
 
 def get_config():
     return CONFIG
 
+
+def get_current_directory():
+    return os.path.dirname(os.path.realpath(__file__))
+
 def get_root_directory():
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(current_dir, '..', '..')
+    return os.path.join(get_current_directory(), '..', '..')
+
+def get_application_directory():
+    return os.path.join(get_current_directory(), '..')
 
 def get_import_directory():
     import_directory = CONFIG.get("system", "import_directory")
@@ -36,3 +50,13 @@ def get_export_directory():
     export_directory = CONFIG.get("system", "export_directory")
     root_directory = get_root_directory()
     return os.path.join(root_directory, export_directory)
+
+def get_default_config_file_path():
+    config_file_name = 'dycast.config'
+    application_directory = get_application_directory()
+    return os.path.join(application_directory, config_file_name)
+
+def get_alembic_config_path():
+    config_file_name = 'alembic.ini'
+    application_directory = get_application_directory()
+    return os.path.join(application_directory, 'init', 'migrations', config_file_name)
